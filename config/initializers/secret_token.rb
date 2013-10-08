6ec90d39474c3c2c9a6a24ff6c349c503dfb20dd
asides <yaroslav.str@gmail.com>
2013-10-08 10:34:11 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Supergum::Application.config.secret_token = 'b17dfd7089d7ac565389ae4ea30f37e4919bcabc47e7c5d96485938efac89d4842ddc88ad8eea444cab51aec575648415ffcf9e73688f637f167495c1be105c2'
